0f58b7c374fffb65cb8d3dc43c8abee85480fb94
vinh <trankhacvinh1991@gmail.com>
2020-01-13 22:26:02 +0700

    căn giữa

--- M/Program.cs
+++ M/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,14 +14,20 @@ namespace M
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    //.UseKestrel()
+                    // .UseContentRoot(Directory.GetCurrentDirectory())
+                    // .UseIISIntegration()
+                     .UseStartup<Startup>();
                 });
     }
 }
